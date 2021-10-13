b034b3dc9ccb62ed2eee9840bbdc11a692f5de19
Jorge Alberto Becerra <jorge.bvlqz@gmail.com>
2021-10-12 19:55:03 -0500

    Add header-only libraries CMake template

---
+++ CMake/header-only-library/header-only-library.h
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void headerOnlyLibraryFunction()
+{
+    std::cout << "Inside headerOnlyLibraryFunction" << std::endl;
+}
--- CMake/template-project/main.cpp
+++ CMake/template-project/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "header-only-library.h"
 
 int main(int argc, char **argv)
 {
     std::cout << "Hello World" << std::endl;
+    headerOnlyLibraryFunction();
 }
